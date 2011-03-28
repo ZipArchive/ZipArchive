c3ed902615e45b2a00c24f5230cdcb64cc5582c8
Sam Soffes <sam@samsoff.es>
2011-03-28 11:59:54 -0700

    Fixed warning

--- minizip/mztools.c
+++ minizip/mztools.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include "zlib.h"
 #include "unzip.h"
+#include "mztools.h"
 
 #define READ_8(adr)  ((unsigned char)*(adr))
 #define READ_16(adr) ( READ_8(adr) | (READ_8(adr+1) << 8) )
