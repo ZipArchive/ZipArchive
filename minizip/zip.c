76e8bf5e0b0c6087fa1285f901127007f5a5577e
Sam Soffes <sam@samsoff.es>
2011-06-21 10:46:27 -0700

    Fixed analyzer issue

--- minizip/zip.c
+++ minizip/zip.c
@@ -1051,8 +1051,8 @@ extern int ZEXPORT zipCloseFileInZipRaw (file, uncompressed_size, crc32)
         uLong uTotalOutBefore;
         if (zi->ci.stream.avail_out == 0)
         {
-            if (zipFlushWriteBuffer(zi) == ZIP_ERRNO)
-                err = ZIP_ERRNO;
+//          if (zipFlushWriteBuffer(zi) == ZIP_ERRNO)
+//              err = ZIP_ERRNO;
             zi->ci.stream.avail_out = (uInt)Z_BUFSIZE;
             zi->ci.stream.next_out = zi->ci.buffered_data;
         }
