e2f4fc164504f7559ab0bf846b8072793bf3d198
Johannes Ekberg <uppfinnarn@gmail.com>
2012-01-14 20:46:32 +0100

    Added Delegation Support

---
+++ SSZipArchiveDelegate.h
@@ -0,0 +1,18 @@
+//
+//  SSZipArchiveDelegate.h
+//  
+//
+//  Created by Johannes Ekberg on 2012-01-14.
+//  Copyright (c) 2012 MacaroniCode Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "minizip/unzip.h"
+@class SSZipArchive;
+
+@protocol SSZipArchiveDelegate <NSObject>
+
+- (void)zipArchiveWillUnzipFile:(NSString *)path globalInfo:(unz_global_info)header;
+- (void)zipArchiveWillUnzipFileNumber:(NSInteger)number fromFile:(NSString *)path fileInfo:(unz_file_info)header;
+
+@end
