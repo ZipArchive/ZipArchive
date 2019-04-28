b0102956011b436fdd09b00d2237ed0bd3b27aa0
Antoine Cœur <antoine.coeur@ef.com>
2019-04-28 10:33:42 +0800

    Adding an example macOS target

--- Example/SwiftExample_macOS/ViewController.swift
+++ Example/SwiftExample_macOS/ViewController.swift
@@ -1,48 +1,52 @@
 //
 //  ViewController.swift
-//  SwiftExample
+//  SwiftExample_macOS
 //
-//  Created by Sean Soper on 10/23/15.
-//
+//  Created by Antoine Cœur on 2019/4/26.
 //
 
-import UIKit
+import Cocoa
 
 #if UseCarthage
-    import ZipArchive
+import ZipArchive
 #else
-    import SSZipArchive
+import SSZipArchive
 #endif
 
-class ViewController: UIViewController {
+class ViewController: NSViewController {
 
-    @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var zipButton: UIButton!
-    @IBOutlet weak var unzipButton: UIButton!
-    @IBOutlet weak var hasPasswordButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-
-    @IBOutlet weak var file1: UILabel!
-    @IBOutlet weak var file2: UILabel!
-    @IBOutlet weak var file3: UILabel!
-    @IBOutlet weak var info: UILabel!
-
+    @IBOutlet weak var passwordField: NSTextField!
+    @IBOutlet weak var zipButton: NSButton!
+    @IBOutlet weak var unzipButton: NSButton!
+    @IBOutlet weak var hasPasswordButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
+    @IBOutlet weak var file1: NSTextField!
+    @IBOutlet weak var file2: NSTextField!
+    @IBOutlet weak var file3: NSTextField!
+    @IBOutlet weak var info: NSTextField!
+    
     var zipPath: String?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         resetPressed(resetButton)
     }
-
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
     // MARK: IBAction
-
-    @IBAction func zipPressed(_: UIButton) {
-        let sampleDataPath = Bundle.main.bundleURL.appendingPathComponent("Sample Data").path
+    
+    @IBAction func zipPressed(_: NSButton) {
+        let sampleDataPath = Bundle.main.bundleURL.appendingPathComponent("Contents/Resources/Sample Data").path
         zipPath = tempZipPath()
-        let password = passwordField.text ?? ""
-
+        let password = passwordField.stringValue
+        
         let success = SSZipArchive.createZipFile(atPath: zipPath!,
                                                  withContentsOfDirectory: sampleDataPath,
                                                  keepParentDirectory: false,
@@ -52,26 +56,26 @@ class ViewController: UIViewController {
                                                  progressHandler: nil)
         if success {
             print("Success zip")
-            info.text = "Success zip"
+            info.stringValue = "Success zip"
             unzipButton.isEnabled = true
             hasPasswordButton.isEnabled = true
         } else {
             print("No success zip")
-            info.text = "No success zip"
+            info.stringValue = "No success zip"
         }
         resetButton.isEnabled = true
     }
-
-    @IBAction func unzipPressed(_: UIButton) {
+    
+    @IBAction func unzipPressed(_: NSButton) {
         guard let zipPath = self.zipPath else {
             return
         }
-
+        
         guard let unzipPath = tempUnzipPath() else {
             return
         }
-
-        let password = passwordField.text ?? ""
+        
+        let password = passwordField.stringValue
         let success: Bool = SSZipArchive.unzipFile(atPath: zipPath,
                                                    toDestination: unzipPath,
                                                    preserveAttributes: true,
@@ -84,74 +88,74 @@ class ViewController: UIViewController {
                                                    completionHandler: nil)
         if success != false {
             print("Success unzip")
-            info.text = "Success unzip"
+            info.stringValue = "Success unzip"
         } else {
             print("No success unzip")
-            info.text = "No success unzip"
+            info.stringValue = "No success unzip"
             return
         }
-
+        
         var items: [String]
         do {
             items = try FileManager.default.contentsOfDirectory(atPath: unzipPath)
         } catch {
             return
         }
-
+        
         for (index, item) in items.enumerated() {
             switch index {
             case 0:
-                file1.text = item
+                file1.stringValue = item
             case 1:
-                file2.text = item
+                file2.stringValue = item
             case 2:
-                file3.text = item
+                file3.stringValue = item
             default:
                 print("Went beyond index of assumed files")
             }
         }
-
+        
         unzipButton.isEnabled = false
     }
-
-    @IBAction func hasPasswordPressed(_: UIButton) {
+    
+    @IBAction func hasPasswordPressed(_: NSButton) {
         guard let zipPath = zipPath else {
             return
         }
         let success = SSZipArchive.isFilePasswordProtected(atPath: zipPath)
         if success {
             print("Yes, it's password protected.")
-            info.text = "Yes, it's password protected."
+            info.stringValue = "Yes, it's password protected."
         } else {
             print("No, it's not password protected.")
-            info.text = "No, it's not password protected."
+            info.stringValue = "No, it's not password protected."
         }
     }
-
-    @IBAction func resetPressed(_: UIButton) {
-        file1.text = ""
-        file2.text = ""
-        file3.text = ""
-        info.text = ""
+    
+    @IBAction func resetPressed(_: NSButton) {
+        file1.stringValue = ""
+        file2.stringValue = ""
+        file3.stringValue = ""
+        info.stringValue = ""
         zipButton.isEnabled = true
         unzipButton.isEnabled = false
         hasPasswordButton.isEnabled = false
         resetButton.isEnabled = false
     }
-
+    
     // MARK: Private
-
+    
     func tempZipPath() -> String {
         var path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0]
         path += "/\(UUID().uuidString).zip"
         return path
     }
-
+    
     func tempUnzipPath() -> String? {
         var path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0]
         path += "/\(UUID().uuidString)"
         let url = URL(fileURLWithPath: path)
-
+        
         do {
             try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
         } catch {
@@ -159,5 +163,4 @@ class ViewController: UIViewController {
         }
         return url.path
     }
-    
 }
