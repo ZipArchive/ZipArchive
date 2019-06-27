787272465c713769201d45389dcfcc569e872f66
PondokiOS (Muhammmad Muizzsuddin) <muiz.idn@gmail.com>
2019-06-27 13:46:49 +0700

    Define SPM project in Package.swift
    Use SPMZipArchive when performs swift package generete-xcodeproj --output SPMZipArchive.xcodeproj
    ziparc is just to ensure ZipArchive is accessible through Swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZipArchive",
+    platforms: [
+      .macOS(.v10_10)
+    ],
+    products: [
+        .executable(
+            name: "ziparc",
+            targets: ["ziparc"]),
+        .library(
+            name: "ZipArchive",
+            targets: ["ZipArchive"]),
+    ],
+    targets: [
+        .target(
+            name: "ziparc",
+            dependencies: ["ZipArchive"],
+            path: "ziparc"),
+        .target(
+            name: "ZipArchive",
+            path: "SSZipArchive")
+    ]
+)
+
