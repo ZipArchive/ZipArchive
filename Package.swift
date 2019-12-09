3557bc56d7cd04ea835ba1f58168425bb1330afa
Vojtech Pajer <vojtech.pajer@synetech.cz>
2019-12-09 15:19:57 +0100

    Removed unused test target.

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ZipArchive",
-            dependencies: []),
-        .testTarget(
-            name: "ZipArchiveTests",
-            dependencies: ["ZipArchive"]),
+            dependencies: [])
     ]
 )
