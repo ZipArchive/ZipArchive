d0e15983ebd0a737e8982bd363fac345d5c26fb0
willson-chen <willson.chenwx@gmail.com>
2020-03-26 22:08:31 +0800

    Fix the problem that ZipArchive compressed special characters are decompressed with 7zip or WinRAR tool display garbled characters because Unicode bit added by PKZIP higher version is not decalared in the zip file header

--- SSZipArchive/SSZipArchive.m
+++ SSZipArchive/SSZipArchive.m
@@ -1134,7 +1134,8 @@ int _zipOpenEntry(zipFile entry, NSString *name, const zip_fileinfo *zipfi, int 
 {
     // https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
     uint16_t made_on_darwin = 19 << 8;
-    return zipOpenNewFileInZip5(entry, name.fileSystemRepresentation, zipfi, NULL, 0, NULL, 0, NULL, Z_DEFLATED, level, 0, -MAX_WBITS, DEF_MEM_LEVEL, Z_DEFAULT_STRATEGY, password.UTF8String, aes, made_on_darwin, 0, 0);
+    uint16_t flag_base = 1 << 11;
+    return zipOpenNewFileInZip5(entry, name.fileSystemRepresentation, zipfi, NULL, 0, NULL, 0, NULL, Z_DEFLATED, level, 0, -MAX_WBITS, DEF_MEM_LEVEL, Z_DEFAULT_STRATEGY, password.UTF8String, aes, made_on_darwin, flag_base, 0);
 }
 
 #pragma mark - Private tools for file info
