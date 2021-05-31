6047e0797304b70cd27bbfe8dd5c59e84249bf58
Joshua Hudson <joshua@jhudsonsoftware.com>
2021-05-31 10:58:15 -0700

    small tweak for SPM path

--- SSZipArchive/include/ZipArchive.h
+++ SSZipArchive/include/ZipArchive.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SSZipArchive.h>
 
-#import "SSZipArchive.h"
+#import "../SSZipArchive.h"
